d41d7e37600e0e330c4951ff3ea53f5bde5a47e9
Marcin Knyc <maroxesen@gmail.com>
2023-11-04 14:07:57 +0100

    add alembic and initial migration to postgres

---
+++ job_search_postgres/models.py
@@ -0,0 +1,40 @@
+from sqlalchemy import Column, Integer, Float, ForeignKey, DateTime, Text
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Link(Base):
+    __tablename__ = 'links'
+    __table_args__ = {'schema': 'links'}
+
+    id = Column(Integer, primary_key=True)
+    link = Column(Text, nullable=False, unique=True)
+    date_added = Column(DateTime, nullable=False)
+
+    offers = relationship("Offer", back_populates="link")
+
+
+class Offer(Base):
+    __tablename__ = 'offers'
+    __table_args__ = {'schema': 'offers'}
+
+    id = Column(Integer, primary_key=True)
+    title = Column(Text, nullable=False)
+    hiring_organization = Column(Text, nullable=False)
+    date_posted = Column(DateTime, nullable=False)
+    valid_through = Column(DateTime, nullable=False)
+    address_country = Column(Text, nullable=False)
+    address_region = Column(Text, nullable=False)
+    address_locality = Column(Text, nullable=False)
+    postal_code = Column(Text)
+    street_address = Column(Text)
+    employment_type = Column(Text, nullable=False)
+    industry = Column(Text, nullable=False)
+    base_salary = Column(Float)
+    job_benefits = Column(Text)
+    responsibilities = Column(Text, nullable=False)
+    experience_requirements = Column(Text, nullable=False)
+    link_id = Column(Integer, ForeignKey('links.links.id'))
+
+    link = relationship("Link", back_populates="offers")
---
+++ job_search_postgres/set_connection_variables_debug.ps1
@@ -0,0 +1,5 @@
+$Env:FIND_A_JOB_USER = "job_search_admin"
+$Env:FIND_A_JOB_PASSWORD = "i_want_you_to_take_me_out_!(+)"
+$Env:FIND_A_JOB_HOST = "localhost"
+$Env:FIND_A_JOB_PORT = 15432
+$Env:FIND_A_JOB_DBNAME = "pracuj"
