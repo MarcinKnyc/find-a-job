9b6b650113b871ec833c4189332526383f831b13
Marcin Knyc <maroxesen@gmail.com>
2023-11-20 14:23:00 +0100

    fix dockerfile

--- job_search_streamlit/init.sh
+++ job_search_streamlit/init.sh
@@ -2,6 +2,6 @@
 
 git clone https://github.com/MarcinKnyc/find-a-job.git
 cd find-a-job/
-pip install -r ./job_search_postgres/requirements.txt
+pip install -r ./job_search_streamlit/requirements.txt
 pip install -e ./job_search_qdrant/
 pip install -e ./job_search_postgres/
